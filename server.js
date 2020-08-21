09243a5c0323bbbee763d8d6e36be4bf6d285175
Voltis <v.agolli@live.com>
2020-08-21 14:56:02 +0200

    added images field

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import { PortfolioAPI } from './datasource';
-import { ApolloServer, gql } from 'apollo-server';
+import { PortfolioAPI } from "./datasource";
+import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type certifications {
@@ -31,6 +31,7 @@ const typeDefs = gql`
     present: Boolean
     description: String
     address: String
+    image: String
   }
 
   type social {
@@ -62,18 +63,18 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     getPortfolio: (root, args, { dataSources }) =>
-      dataSources.portfolioAPI.getPortfolio()
-  }
+      dataSources.portfolioAPI.getPortfolio(),
+  },
 };
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
-    portfolioAPI: new PortfolioAPI()
+    portfolioAPI: new PortfolioAPI(),
   }),
   introspection: true,
-  playground: true
+  playground: true,
 });
 
 debugger;
